ba5e499b0f981c330f7bdfae1d063dd7d1146233
Amrita <Amrita@DESKTOP-E12F0AK>
2025-09-16 15:21:33 +0530

    NEW

---
+++ ARRAY/array3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.ARRAY
+{
+    internal class array3
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter the elements:");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Reverse(arr);
+
+            Console.WriteLine("Elements in reverse order:");
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ ARRAY/array4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.ARRAY
+{
+    internal class array4
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] sourceArray = new int[n];
+            int[] destinationArray = new int[n];
+
+            Console.WriteLine("Enter the elements of the array:");
+            for (int i = 0; i < n; i++)
+            {
+                sourceArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Copy elements
+            for (int i = 0; i < n; i++)
+            {
+                destinationArray[i] = sourceArray[i];
+            }
+
+            Console.WriteLine("\nElements copied to the new array:");
+            foreach (int item in destinationArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
