bb09a168f1310124f24bc139adbe39bae5a35ce6
Amrita <Amrita@DESKTOP-E12F0AK>
2025-09-16 15:25:38 +0530

    NEW

---
+++ ARRAY/array7.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.ARRAY
+{
+    internal class array7
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+            int[] evenArr = new int[n];
+            int[] oddArr = new int[n];
+
+            int evenCount = 0, oddCount = 0;
+
+            Console.WriteLine("Enter the elements:");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Separate into even and odd arrays
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    evenArr[evenCount] = arr[i];
+                    evenCount++;
+                }
+                else
+                {
+                    oddArr[oddCount] = arr[i];
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine("\nEven elements:");
+            for (int i = 0; i < evenCount; i++)
+            {
+                Console.Write(evenArr[i] + " ");
+            }
+
+            Console.WriteLine("\nOdd elements:");
+            for (int i = 0; i < oddCount; i++)
+            {
+                Console.Write(oddArr[i] + " ");
+            }
+        }
+    }
+}
---
+++ ARRAY/array9.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.ARRAY
+{
+    internal class array9
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter the elements:");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Sort in descending order
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            Console.WriteLine("\nArray in descending order:");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
