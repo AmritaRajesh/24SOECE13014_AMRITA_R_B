36e59414f5929cb99e795f3564a3d26d01bca4f1
Amrita <Amrita@DESKTOP-E12F0AK>
2025-09-16 15:38:09 +0530

    NEW

---
+++ BHOMI MAM/pra2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.BHOMI_MAM
+{
+    internal class pra2
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(@"
+                Name: Amrita,
+                DOB: 29/07/2004,
+                Address: Rameshwar, Patel Colony
+                City: Jamnagar
+                Pincode: 361008
+                State: Gujarat
+                Country: India
+                Email: amrita@gmail.com");
+        }
+    }
+}
---
+++ BHOMI MAM/pra4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.BHOMI_MAM
+{
+    internal class pra4
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.Write("Enter Number : ");
+            string str = Console.ReadLine();
+            x = Convert.ToInt32(str);
+            if (x % 2 == 0)
+                Console.WriteLine("Number is Even");
+            else
+                Console.WriteLine("Number is Odd");
+            Console.ReadLine();
+        }
+    }
+}
