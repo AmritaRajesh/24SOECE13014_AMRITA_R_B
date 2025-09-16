85fac0b71f94ee397f29fad136b969c461193ba3
Amrita <Amrita@DESKTOP-E12F0AK>
2025-09-16 15:40:07 +0530

    NEW

---
+++ BHOMI MAM/pra6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.BHOMI_MAM
+{
+    internal class pra6
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, result;
+            string str;
+            Console.Write("Enter Number 1: ");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+            Console.Write("Enter Number 2: ");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+            Console.Write("Enter Number 3: ");
+            str = Console.ReadLine();
+            c = Convert.ToInt32(str);
+            result = Sum(a, b, c);
+            Console.WriteLine($"Sum: {result}");
+            Console.Read();
+        }
+        static int Sum(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+    }
+}
---
+++ BHOMI MAM/pra8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.BHOMI_MAM
+{
+    internal class pra8
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            string upperName = name.ToUpper();
+            Console.WriteLine($"Uppercase Name: {upperName}");
+            Console.ReadLine();
+        }
+    }
+}
