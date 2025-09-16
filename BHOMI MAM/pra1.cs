292fd515ae1bc8dcef9c866a461a3c2a0b7245dd
Amrita <Amrita@DESKTOP-E12F0AK>
2025-09-16 15:36:32 +0530

    NEW

---
+++ BHOMI MAM/pra1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.BHOMI_MAM
+{
+    internal class pra1
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Hello World!!");
+
+            System.Console.Read();
+        }
+    }
+}
