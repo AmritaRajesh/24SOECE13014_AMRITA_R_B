7ac8e57d9710d08fbb9306a7c5be2c7a8880c08e
Amrita <Amrita@DESKTOP-E12F0AK>
2025-09-16 15:22:54 +0530

    NEW

---
+++ ARRAY/array5.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.ARRAY
+{
+    internal class array5
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter the elements:");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int duplicateCount = 0;
+
+            // Count duplicate elements
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        duplicateCount++;
+                        break; // Avoid counting same element multiple times
+                    }
+                }
+            }
+            Console.WriteLine("\nTotal number of duplicate elements: " + duplicateCount);
+        }
+    }
+}
---
+++ ARRAY/array6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA_R_B.ARRAY
+{
+    internal class array6
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("Enter the elements:");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            // Initialize max and min with the first element
+            int max = arr[0];
+            int min = arr[0];
+            // Find max and min
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("\nMaximum element: " + max);
+            Console.WriteLine("Minimum element: " + min);
+        }
+    }
+}
